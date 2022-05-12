038fd65452537dfbc82a87848ef0a2a9eddba8f5
eduardo-ahuir <ahuir61@gmail.com>
2022-05-12 17:30:00 +0200

    La base esta casi lista

--- main.py
+++ main.py
@@ -4,19 +4,21 @@ import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
 
 con = sqlite3.connect('bdmonstruos.db')
 cur = con.cursor()
+print("conexion realizada")
 # Instantiate options
-driver = webdriver.Chrome(ChromeDriverManager().install())
-
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get("https://www.google.com")
 # Load the HTML page
 driver.get("https://www.aidedd.org/dnd-filters/monsters.php")
-
+time.sleep(20)
 # Parse processed webpage with BeautifulSoup
 soup = BeautifulSoup(driver.page_source, features="lxml")
-time.sleep(5)
-for td in soup.find_all('tbody', class_="item"):
+for td in soup.find_all('td', class_="item"):
     f = open('output3.txt', 'a')
     valor = str(td.get_text()).replace("'","")
     f.write(td.get_text())
