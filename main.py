c5e08e86c4c12368022334b7376b82684b0efee1
eduardo-ahuir <ahuir61@gmail.com>
2022-05-12 16:52:28 +0200

    Hay un error raro

--- main.py
+++ main.py
@@ -1,34 +1,29 @@
 # -*- coding: utf-8 -*-
 import sqlite3
-
+import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
+con = sqlite3.connect('bdmonstruos.db')
+cur = con.cursor()
+# Instantiate options
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
-class scrapper():
-    # Instantiate options
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+# Load the HTML page
+driver.get("https://www.aidedd.org/dnd-filters/monsters.php")
 
-    # Load the HTML page
-    driver.get("https://www.aidedd.org/dnd-filters/monsters.php")
-
-    # Parse processed webpage with BeautifulSoup
-    soup = BeautifulSoup(driver.page_source, features="lxml")
-
-    for td in soup.find_all('td', class_="item"):
-        f = open('output3.txt', 'a')
-        valor = td.get_text()
-        f.write(td.get_text())
-        f.close()
-
-
-class db():
-    con = sqlite3.connect('bdmonstruos.db')
-    cur = con.cursor()
-    sc = scrapper()
+# Parse processed webpage with BeautifulSoup
+soup = BeautifulSoup(driver.page_source, features="lxml")
+time.sleep(5)
+for td in soup.find_all('tbody', class_="item"):
+    f = open('output3.txt', 'a')
+    valor = str(td.get_text()).replace("'","")
+    f.write(td.get_text())
+    f.close()
     # Insert a row of data
-    cur.execute("INSERT INTO mounstruos VALUES (" + sc.valor + ")")
+    print("insertando"+" "+valor)
+    cur.execute("INSERT INTO mounstruos VALUES ("+"'"+valor+"'"+")")
     # Save (commit) the changes
     con.commit()
-    con.close()
+con.close()
