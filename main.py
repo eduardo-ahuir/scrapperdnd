2a1bf0c10f767be2e6dd6529492dc3232b98797d
eduardo-ahuir <ahuir61@gmail.com>
2022-05-12 22:42:04 +0200

    La base esta casi lista

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 import sqlite3
 import time
+
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
-
+from webdriver_manager.chrome import ChromeDriverManager
 
 con = sqlite3.connect('bdmonstruos.db')
 cur = con.cursor()
@@ -15,17 +15,17 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get("https://www.google.com")
 # Load the HTML page
 driver.get("https://www.aidedd.org/dnd-filters/monsters.php")
-time.sleep(20)
+time.sleep(0)
 # Parse processed webpage with BeautifulSoup
 soup = BeautifulSoup(driver.page_source, features="lxml")
-for td in soup.find_all('td', class_="item"):
-    f = open('output3.txt', 'a')
-    valor = str(td.get_text()).replace("'","")
-    f.write(td.get_text())
-    f.close()
-    # Insert a row of data
-    print("insertando"+" "+valor)
-    cur.execute("INSERT INTO mounstruos VALUES ("+"'"+valor+"'"+")")
-    # Save (commit) the changes
-    con.commit()
+
+for td in soup.find_all("td", class_="item"):
+    valor = str(td.get_text()).replace("'", "")
+    print(valor)
+    cur.execute('INSERT INTO mounstruos(nombre) VALUES('+valor+"'"+")")
+for td in soup.find_all("td", class_="center"):
+    valor = str(td.get_text()).replace("'", "")
+    print(valor)
+    cur.execute('INSERT INTO mounstruos(cr) VALUES(' + valor + "'" + ")")
+con.commit()
 con.close()
